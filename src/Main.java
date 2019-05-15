58589a1e9bab70eb2cc6610c1ac7ca4ca7b27c74
johnlinder92 <john.linder92@gmail.com>
2019-05-15 19:37:25 +0200

    Flyttat seleniumtestet till en seleniumtestfil och börjat bygga main scriptet(för att kolla pris för 1 paket) genom att öppna upp ups hemsidan.

--- src/Main.java
+++ src/Main.java
@@ -10,28 +10,18 @@ public class Main {
         System.setProperty("webdriver.chrome.driver","C:\\Program Files (x86)\\Fraktautomatition- The Humble Co\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
-        String baseUrl = "http://demo.guru99.com/test/newtours/";
-        String expectedTitle = "Welcome: Mercury Tours";
-        String actualTitle = "";
+        String baseUrl = "https://www.ups.com/ship/guided/origin?tx=4650480306877647&loc=en_SE";
+
 
         // launch Fire fox and direct it to the Base URL
         driver.get(baseUrl);
 
-        // get the actual value of the title
-        actualTitle = driver.getTitle();
 
-        /*
-         * compare the actual title of the page with the expected one and print
-         * the result as "Passed" or "Failed"
-         */
-        if (actualTitle.contentEquals(expectedTitle)){
-            System.out.println("Test Passed!");
-        } else {
-            System.out.println("Test Failed");
-        }
 
-        //close Chrome
-        driver.close();
+
+
+
+
 
     }
 
