48072f3842f7dbfdd12e6231c564bee064a06703
johnlinder92 <john.linder92@gmail.com>
2019-05-17 17:19:22 +0200

    Lagt till enhetstest som kollar att postnummer kan komma från scannern(världen utanför programmet) likväl som att man skriver in värdet direkt i metoden.

--- Test/MainTest.java
+++ Test/MainTest.java
@@ -1,2 +1,36 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MainTest {
+
+
+    // testar att metoden som beräknar pris kan ta input från kund(scannern) i detta fall endast postnummer.
+
+
+    @Test
+    public void getUpsPrice_ChecksInputValueisUsed_ChecksThatPostnummertValueIsSameAsIfValueWereGivenLocallytoPostnummer() {
+
+        InputStream inputStream = MainTest.class.getResourceAsStream("/testpostnummervalue.txt");
+        Scanner scanner = new Scanner(inputStream);
+        ConsoleHandler ch = new ConsoleHandler(scanner);
+
+       Getinfo info = new Getinfo();
+       Getprices gp = new Getprices();
+
+       String pris = gp.getUpsPrice(info.Postnummerinfo());
+       String pris2 = gp.getUpsPrice(18164);
+
+
+
+        assertEquals(pris, pris2);
+
+
+    }
+
+
+
 }
